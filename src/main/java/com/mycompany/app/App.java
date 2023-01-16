488fe12e00a5f56ee72148fce3196a8e7380a89b
Rachel <tyng_88@hotmail.com>
2023-01-16 16:43:50 +0800

    My second change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hate Jenkins!!");
+        System.out.println("Love Jenkins!!");
     }
 
     public String getMessage() {
